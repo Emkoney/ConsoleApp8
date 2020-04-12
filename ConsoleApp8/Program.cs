61921495b6d5a10a1923a880ff2217f75d09087d
Maximus <6383127@gmail.com>
2020-04-12 16:06:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object o = "fsdf";
+
+            if (o is string x)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+
+
+}
+
+
+
+
